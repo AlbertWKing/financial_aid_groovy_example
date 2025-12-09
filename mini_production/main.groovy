549b24500b6d68c7136a140fa4ffe4b033305b7e
Albert King <thealbertking@gmail.com>
2025-12-08 20:50:43 -0500

    fix export file path error

--- mini_production/main.groovy
+++ mini_production/main.groovy
@@ -5,7 +5,6 @@ class Main {
 
         def csv_output = new output_to_csv()
         println "Working directory: " + new File(".").absolutePath
-        csv_output.write_to_csv(results, "./Groovy/fin_aid_example/mini_production/example_students.csv")
         csv_output.write_to_csv(results, "/Users/albert/Documents/Groovy/fin_aid_example/mini_production/example_students.csv")
 
         println "Data exported successfully."
