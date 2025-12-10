91e9786cf6ee27096f1f4d2bbb13b78dbb8129f7
Albert King <thealbertking@gmail.com>
2025-12-10 09:47:26 -0500

    added more complex SQL, check if table join works

--- mini_production/main.groovy
+++ mini_production/main.groovy
@@ -1,6 +1,23 @@
 class Main {
     static void main(String[] args) {
-        String query = "SELECT * FROM student_bio LIMIT 15;"
+        // String query = """
+        // SELECT *
+        // FROM student_bio
+        // LIMIT 10;"""
+
+        String query = """
+        SELECT bio.student_id AS "Student ID", detail.award_type AS "Award Type", detail.award_amount AS "Award Amount"
+        FROM student_bio AS bio
+        JOIN student_award AS detail
+        ON bio.student_id = detail.student_id
+        LIMIT 15;"""
+
+        // String query = """
+        // SELECT bio.student_id as "Student ID", detail.award_type as "Award Type", detail.award_amount as "Award Amount"
+        // FROM student_bio AS bio
+        // JOIN student_award as detail
+        // ON bio.student_id = detail.student_id
+        // LIMIT 15;"""
 
         def sql_query = new run_sql()
         def results = sql_query.run_query(query)
